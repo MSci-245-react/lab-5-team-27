79af711aab0c06b65c2a5f1379fd5e50e145fdce
Nicholas Chia <nicholas.chia.ca@gmail.com>
2024-02-08 10:31:48 -0500

    set up Cypress E2E Tests

--- cypress/integration/smoke.cy.js
+++ cypress/integration/smoke.cy.js
@@ -1,9 +1,3 @@
-describe('empty spec', () => {
-  it('can view the home page', () => {
-    cy.visit('/');
-    cy.contains('Learn React');
-  });
-});
 
 describe('Smoke Test', () => {
   it('can view the home page', () => {
