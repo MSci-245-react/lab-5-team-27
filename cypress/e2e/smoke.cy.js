f0a386121efc9588c482e9f66039ac4a5ef5eac4
Nicholas Chia <nicholas.chia.ca@gmail.com>
2024-02-08 19:20:23 -0500

    Testing

--- cypress/e2e/smoke.cy.js
+++ cypress/e2e/smoke.cy.js
@@ -1,13 +1,6 @@
-describe('empty spec', () => {
-  it('can view the home page', () => {
-    cy.visit('/');
-    cy.contains('Learn React');
-  });
-});
-
-describe('Smoke Test', () => {
-  it('can view the home page', () => {
-    cy.visit('/');
-    cy.contains('Learn React');
-  });
-});
+// describe('empty spec', () => {
+//   it('can view the home page', () => {
+//     cy.visit('/');
+//     cy.contains('Learn React');
+//   });
+// });
